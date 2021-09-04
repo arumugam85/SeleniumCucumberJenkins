e5b1417a96b48781af8682a606919f53ebb1cd3b
arumugam85 <rarumugambe@gmail.com>
2021-09-04 12:54:00 -0400

    first commit for selenium demo project

---
+++ src/test/java/com/qascript/SeleniumDemo.java
@@ -0,0 +1,37 @@
+package com.qascript;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumDemo {
+
+
+    @Test
+    public void OpenBrowser() throws InterruptedException  {
+        WebDriver driver;
+        //System.setProperty("webdriver.chrome.driver", "A:\\Softwares\\ChromeDriver\\chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver(chromeOptions);
+		driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        Thread.sleep(3000);
+        driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+        driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+        driver.findElement(By.id("btnLogin")).click();
+        Thread.sleep(3000);        
+        System.out.println("Title of the page is: " + driver.getTitle());
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("OrangeHRM"));
+        driver.quit();
+    }
+
+
+}
+
+
