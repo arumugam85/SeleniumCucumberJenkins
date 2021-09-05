ad0af0891a653d0cbca5a1f17f22ec4cc18765f7
arumugam85 <rarumugambe@gmail.com>
2021-09-05 11:27:01 -0400

    Browser parameter updated

--- src/test/java/com/qascript/SeleniumDemo.java
+++ src/test/java/com/qascript/SeleniumDemo.java
@@ -1,43 +1,59 @@
 package com.qascript;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumDemo {
 
+	@Parameters("Browser")
+	@Test
+	public void OpenBrowser(String browser) throws InterruptedException {
 
-    @Test
-    public void OpenBrowser() throws InterruptedException  {
-        WebDriver driver;
-        //System.setProperty("webdriver.chrome.driver", "A:\\Softwares\\ChromeDriver\\chromedriver.exe");
-        ChromeOptions chromeOptions = new ChromeOptions();
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver(chromeOptions);
+		System.out.println("Parameter Name is " + browser);
+		WebDriver driver = null;
+
+		if (browser.contains("Chrome")) {
+			ChromeOptions chromeOptions = new ChromeOptions();
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver(chromeOptions);
+			System.out.println("Launch Chrome Browser");
+		} else if (browser.contains("FireFox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			System.out.println("Launch FF Browser");
+		}
+
+		else if (browser.contains("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			System.out.println("Launch Edge Browser");
+		}
+
 		driver.manage().window().maximize();
-		System.out.println("Launch Browser");
-        driver.get("https://opensource-demo.orangehrmlive.com/");
-        Thread.sleep(3000);
-        System.out.println("Enter Usr name");
-        driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-        System.out.println("Enter password");
-        driver.findElement(By.id("txtPassword")).sendKeys("admin123");
-        driver.findElement(By.id("btnLogin")).click();
-        System.out.println("Submitted");
-        Thread.sleep(4000);        
-        System.out.println("Title of the page is: " + driver.getTitle());
-        Assert.assertEquals(driver.getTitle(), "OrangeHRM");
-        driver.quit();
-        System.out.println("Browser closed");
-    }
+		System.out.println("************Launch OHRM Application**************");
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Thread.sleep(3000);
 
+		System.out.println("************Enter User name************");
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		System.out.println("**********Enter password*********");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		System.out.println("************Submitted****************");
+		Thread.sleep(4000);
+		System.out.println("*********Title of the page is: " + driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), "OrangeHRM");
+		driver.quit();
+		System.out.println("********Browser: " + browser + " closed**********");
+	}
 
 }
-
-
