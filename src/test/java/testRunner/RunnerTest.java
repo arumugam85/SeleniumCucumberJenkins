09370089eecdbbd06461d8c7ee669f0933fa9002
arumugam85 <rarumugambe@gmail.com>
2021-09-07 17:41:23 -0400

    Test Runner updated

--- src/test/java/testRunner/RunnerTest.java
+++ src/test/java/testRunner/RunnerTest.java
@@ -7,31 +7,19 @@ import org.testng.annotations.Test;
 
 import com.vimalselvam.cucumber.listener.Reporter;
 
+import io.cucumber.java.After;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @Test
-@CucumberOptions(        
-        features =  {"src/test/resources/features"},
-        glue=       {"stepDefns"},
-        monochrome = true,
-        strict = true,
-        tags   = {"@login"},
-        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
-        ) 
+@CucumberOptions(features = { "src/test/resources/features" }, 
+		glue = {"stepDefns" },
+		monochrome = true, 
+		strict = true, 
+		tags = { "@login" }, 
+		plugin = { "pretty","html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json" })
 
 public class RunnerTest extends AbstractTestNGCucumberTests {
-	
-	@AfterClass
-	public static void setup()
-	{
-	Reporter.loadXMLConfig(new File("src/test/resources/extent-config.xml"));
-	Reporter.setSystemInfo("Test User", System.getProperty("user.name"));
-	Reporter.setSystemInfo("User Name", "ARU");
-	Reporter.setSystemInfo("Application Name", "OHRM Application");
-	Reporter.setSystemInfo("Operating System Type", System.getProperty("os.name").toString());
-	Reporter.setSystemInfo("Environment", "Production");
-	Reporter.setTestRunnerOutput("Test Execution Cucumber Report");
+
 	}
 
-}
