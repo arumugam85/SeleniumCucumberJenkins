4c6b41372097bab729c56e2aa3f3c6b80df40a31
arumugam85 <rarumugambe@gmail.com>
2021-09-10 12:26:44 -0400

    Test report updated

--- src/test/java/testRunner/RunnerTest.java
+++ src/test/java/testRunner/RunnerTest.java
@@ -1,13 +1,7 @@
 package testRunner;
 
-import java.io.File;
-
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
-import com.vimalselvam.cucumber.listener.Reporter;
-
-import io.cucumber.java.After;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
